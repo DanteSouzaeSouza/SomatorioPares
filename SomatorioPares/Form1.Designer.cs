767e0ce4dff2dbc00b60ba02622bb82c4dedfe3d
Dante Souza e Souza <lokinmodar@gmail.com>
2020-05-04 18:16:40 -0300

    Full code

--- SomatorioPares/Form1.Designer.cs
+++ SomatorioPares/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace SomatorioPares
 {
-    partial class Form1
+    partial class FrmSomatorioPares
     {
         /// <summary>
         /// Required designer variable.
@@ -28,12 +28,92 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.txtNum = new System.Windows.Forms.TextBox();
+            this.txtSoma = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(28, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Número";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(35, 78);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Soma:";
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.Location = new System.Drawing.Point(322, 75);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(75, 23);
+            this.btnLimpar.TabIndex = 2;
+            this.btnLimpar.Text = "Limpar";
+            this.btnLimpar.UseVisualStyleBackColor = true;
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(322, 32);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(75, 23);
+            this.btnCalcular.TabIndex = 3;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // txtNum
+            // 
+            this.txtNum.Location = new System.Drawing.Point(92, 34);
+            this.txtNum.Name = "txtNum";
+            this.txtNum.Size = new System.Drawing.Size(204, 20);
+            this.txtNum.TabIndex = 4;
+            // 
+            // txtSoma
+            // 
+            this.txtSoma.Location = new System.Drawing.Point(92, 75);
+            this.txtSoma.Name = "txtSoma";
+            this.txtSoma.Size = new System.Drawing.Size(204, 20);
+            this.txtSoma.TabIndex = 5;
+            // 
+            // FrmSomatorioPares
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(420, 133);
+            this.Controls.Add(this.txtSoma);
+            this.Controls.Add(this.txtNum);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "FrmSomatorioPares";
+            this.Text = "Digite a quantidade de números pares a somar!";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnLimpar;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.TextBox txtNum;
+        private System.Windows.Forms.TextBox txtSoma;
     }
 }
 
--- SomatorioPares/Form1.cs
+++ SomatorioPares/Form1.cs
@@ -10,11 +10,46 @@ using System.Windows.Forms;
 
 namespace SomatorioPares
 {
-    public partial class Form1 : Form
+    public partial class FrmSomatorioPares : Form
     {
-        public Form1()
+        public FrmSomatorioPares()
         {
             InitializeComponent();
+        }
+
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            //declaração das variáveis
+            int i, num, soma = 0;
+
+            //entrada de dados - atribui a num o valor do txtNum
+            num = Convert.ToInt32(txtNum.Text);
+
+            //inicializa o laço em 1;
+            //testa a condição de parada: i <= num
+            //incrementa a variavel i++
+            for (i = 1; i <= num; i++)
+            {
+                //verifica se i é par
+                if (i % 2 == 0)
+                {
+                    //faz o somatório dos números pares
+                    soma = soma + i;
+                }
+            }
+
+            //apresenta o total da soma dos pares no txtSoma
+            txtSoma.Text = soma.ToString();
+        }
+
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            //limpa os TextBox
+            txtSoma.Clear();
+            txtNum.Clear();
+            //coloca o foco no txtNum
+            txtNum.Focus();
         }
     }
 }
